529b1f9b322241a7e9924ec75c22cca53fdfdcc1
KuroiGengetsu <1198148133@qq.com>
2018-01-01 22:16:50 +0800

    change struct_test.c

--- README.md
+++ README.md
@@ -636,6 +636,8 @@ struct COMPLEX {
 
 使用方法: **结构体变量名.成员名**
 
+点操作符 `.` 和 下标操作符 `[]` 具有相同的优先级, 所以不必加上括号, 除非你不放心
+
 例子:  源代码见[./examples/struct_test.c](https://github.com/KuroiGengetsu/ReviewC/blob/master/examples/struct_test.c)
 
 ```C
@@ -653,7 +655,7 @@ struct COMPLEX {
     long *lp;
     mystruct s;
     mystruct *sp;
-    mystruct *sy[20];
+    mystruct sy[20];
 };
 
 int main() {
@@ -666,12 +668,14 @@ int main() {
     comp.s.a = 13;  // 访问 comp 的 mystruct类型成员变量 s 的成员变量 a 并赋值 13
     comp.s.b = 'H';  // 访问 comp 的 s 的成员变量 b 并赋值 'H'
     comp.s.c = 3.14;  // 访问 comp 的 s 的成员变量 c 并赋值 3.14
+    comp.sy[0].a = 55;  // 访问 comp 的 mystruct 类型数组 sy 第一个元素 的成员 a 并赋值 55, 其他元素的访问类似
     printf("comp.a = %d, comp.b[5] = %d, *comp.lp = %ld, comp.s.a = %d, comp.s.b = %c, comp.s.c = %g\n",
         comp.a, comp.b[5],
         *comp.lp,  // 注意这里的 `.` 操作符优先级比 间访运算符 `*` 高, 不放心你可以加上括号 *(comp.lp)
         comp.s.a, comp.s.b, comp.s.c);  // 不放心你可以加上括号, 如 (comp.s).a 等等
     return 0;
 }
+
 ```
 
 #### 间接访问
--- examples/struct_test.c
+++ examples/struct_test.c
@@ -12,18 +12,20 @@ struct COMPLEX {
     long *lp;
     mystruct s;
     mystruct *sp;
-    mystruct *sy[20];
+    mystruct sy[20];
 };
 
 int main() {
     long distance = 10000;
-    struct COMPLEX comp;
-    comp.a = 3;
-    comp.b[5] = 17;
-    comp.lp = &distance;
-    comp.s.a = 13;
-    comp.s.b = 'H';
-    comp.s.c = 3.14;
+
+    struct COMPLEX comp; // 声明一个结构体变量
+    comp.a = 3;  // 访问 整型成员变量 a 并赋值 3
+    comp.b[5] = 17;  // 访问整型数组成员变量 b 的第六个元素, 并赋值 17, 你也可以这样写: (comp.b)[5]
+    comp.lp = &distance;  // 将 长整型指针 成员变量 lp 指向 distance
+    comp.s.a = 13;  // 访问 comp 的 mystruct类型成员变量 s 的成员变量 a 并赋值 13
+    comp.s.b = 'H';  // 访问 comp 的 s 的成员变量 b 并赋值 'H'
+    comp.s.c = 3.14;  // 访问 comp 的 s 的成员变量 c 并赋值 3.14
+    comp.sy[0].a = 55;  // 访问 comp 的 mystruct 类型数组 sy 第一个元素 的成员 a 并赋值 55, 其他元素的访问类似
     printf("comp.a = %d, comp.b[5] = %d, *comp.lp = %ld, comp.s.a = %d, comp.s.b = %c, comp.s.c = %g\n",
         comp.a, comp.b[5],
         *comp.lp,  // 注意这里的 `.` 操作符优先级比 间访运算符 `*` 高, 不放心你可以加上括号 *(comp.lp)
